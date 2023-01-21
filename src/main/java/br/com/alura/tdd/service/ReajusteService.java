21e3a923e6b565068c74e84a1789c26dd8b68fc2
mnishimori <mnishimori@yahoo.com.br>
2023-01-21 15:51:55 -0300

    feat[reajuste]: Aplicado o padrão de projeto strategy.

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,15 +7,7 @@ import java.math.BigDecimal;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho.equals(Desempenho.A_DESEJAR)) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.realizarReajuste(reajuste);
-        } else if (desempenho.equals(Desempenho.BOM)){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.realizarReajuste(reajuste);
-        } else if (desempenho.equals(Desempenho.OTIMO)){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.realizarReajuste(reajuste);
-        }
+        var reajuste = funcionario.getSalario().multiply(desempenho.percentualReajuste());
+        funcionario.realizarReajuste(reajuste);
     }
 }
