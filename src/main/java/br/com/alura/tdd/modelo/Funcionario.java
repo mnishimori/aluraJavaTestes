684d1ac32415d51477e0770b37f4e6a4e9a69058
mnishimori <mnishimori@yahoo.com.br>
2023-01-21 15:13:34 -0300

    feat[funcionario] Reajuste do salário do funcionário.

--- src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.modelo;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
 public class Funcionario {
@@ -27,4 +28,7 @@ public class Funcionario {
 		return salario;
 	}
 
+	public void realizarReajuste(BigDecimal reajuste) {
+		this.salario = this.getSalario().add(reajuste).setScale(2, RoundingMode.HALF_UP);
+	}
 }
---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReajusteServiceTest {
+
+    @Test
+    void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar(){
+        var service = new ReajusteService();
+        var funcionario = new Funcionario("Ana", LocalDate.now(),
+                new BigDecimal("1000.00"));
+
+        service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+    }
+}
