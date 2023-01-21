cc00a083d32fabc2dade212a6ef114bca27eeb12
mnishimori <mnishimori@yahoo.com.br>
2023-01-21 17:27:26 -0300

    refactor[funcionário]: Criado método de arredondamento de salário.

--- src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -29,6 +29,11 @@ public class Funcionario {
 	}
 
 	public void realizarReajuste(BigDecimal reajuste) {
-		this.salario = this.getSalario().add(reajuste).setScale(2, RoundingMode.HALF_UP);
+		this.salario = this.getSalario().add(reajuste);
+		this.arrendodarSalario();
+	}
+
+	private void arrendodarSalario() {
+		this.salario = this.salario.setScale(2, RoundingMode.HALF_UP);
 	}
 }
